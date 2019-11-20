d5e8712f7307a9b5a8cc7cd6fcf99f7ebbfe1b81
huazewang <68068068@qq.com>
2019-11-20 11:33:30 +0800

    Initial commit

---
+++ app/src/main/java/ihuaze/yinao/Face/MyApplication.java
@@ -0,0 +1,15 @@
+package ihuaze.yinao.Face;
+
+import android.app.Application;
+
+
+public class MyApplication extends Application {
+
+    private static final String api_key = "2e793bb829af7fbf3aa14f12875368f5";
+    private static final String api_secret = "a9118441d97d64da88e32e220b455594";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
